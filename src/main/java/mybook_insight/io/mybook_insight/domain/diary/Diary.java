279442fd83c9c82900e3fec061224ce4e4f9c578
eunkang-choi <dmsrkd1216@gmail.com>
2025-06-17 03:56:36 +0900

    Fix: Diary엔티티모델 외래키 컬럼조인 옵션 수정 - 논리적외래키로 변경

--- src/main/java/mybook_insight/io/mybook_insight/domain/diary/Diary.java
+++ src/main/java/mybook_insight/io/mybook_insight/domain/diary/Diary.java
@@ -1,7 +1,11 @@
 package mybook_insight.io.mybook_insight.domain.diary;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import mybook_insight.io.mybook_insight.domain.book.Book;
@@ -10,7 +14,10 @@ import mybook_insight.io.mybook_insight.domain.user.User;
 
 @Entity
 @Table(name = "diary")
-@Getter @RequiredArgsConstructor
+@Getter
+@NoArgsConstructor( access = AccessLevel.PROTECTED )
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder(access = AccessLevel.PRIVATE)
 public class Diary extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -30,14 +37,30 @@ public class Diary extends BaseEntity {
 
     // 관계 매핑: Diary - User (N:1)
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "user_id", nullable = false)
+    @JoinColumn(name = "user_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     @Setter
     private User user;
 
     // 관계 매핑: Diary - Book (N:1)
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "book_id", nullable = false)
+    @JoinColumn(name = "book_id", nullable = false, foreignKey = @ForeignKey(ConstraintMode.NO_CONSTRAINT))
     @Setter
     private Book book;
 
+
+    public static Diary of(String title, String thoughts, boolean isFinished, int readPages) {
+        return Diary.builder()
+            .title(title)
+            .thoughts(thoughts)
+            .isFinished(isFinished)
+            .readPages(readPages)
+            .build();
+    }
+    public static Diary of(String title, String thoughts, boolean isFinished) {
+        return Diary.builder()
+            .title(title)
+            .thoughts(thoughts)
+            .isFinished(isFinished)
+            .build();
+    }
 }
