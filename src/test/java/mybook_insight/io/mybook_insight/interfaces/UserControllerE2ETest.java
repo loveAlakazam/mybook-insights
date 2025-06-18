26d162d61e6d261e118cc3e93ee2386bafd1eaff
eunkang-choi <dmsrkd1216@gmail.com>
2025-06-19 05:28:53 +0900

    Test: e2e 테스트케이스 작성

--- src/test/java/mybook_insight/io/mybook_insight/interfaces/UserControllerE2ETest.java
+++ src/test/java/mybook_insight/io/mybook_insight/interfaces/UserControllerE2ETest.java
@@ -1,6 +1,8 @@
 package mybook_insight.io.mybook_insight.interfaces;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
@@ -11,18 +13,26 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.context.annotation.Import;
 import org.springframework.http.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
 
 import mybook_insight.io.mybook_insight.domain.book.UserRole;
+import mybook_insight.io.mybook_insight.domain.user.User;
+import mybook_insight.io.mybook_insight.domain.user.UserRepository;
 import mybook_insight.io.mybook_insight.domain.user.UserService;
 import mybook_insight.io.mybook_insight.infrastructures.TestcontainersConfiguration;
 import mybook_insight.io.mybook_insight.interfaces.user.UserController;
 import mybook_insight.io.mybook_insight.interfaces.user.UserJoinRequest;
 import mybook_insight.io.mybook_insight.interfaces.user.UserJoinResponse;
+import mybook_insight.io.mybook_insight.interfaces.user.UserLoginRequest;
+import mybook_insight.io.mybook_insight.interfaces.user.UserLoginResponse;
 
 @AutoConfigureMockMvc
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(TestcontainersConfiguration.class)
-class UserControllerE2ETest {
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+public class UserControllerE2ETest {
 
 	private static final Logger log = LoggerFactory.getLogger(UserControllerE2ETest.class);
 	@Autowired
@@ -31,6 +41,29 @@ class UserControllerE2ETest {
 	private UserController userController;
 	@Autowired
 	private UserService userService;
+	@Autowired
+	private UserRepository userRepository;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	String existedUserEmail = "user0@email.com";
+	String existedUserRawPassword = "password0";
+	String existedUserNickname = "user0";
+
+	@BeforeEach
+	void setUp() {
+		// 테스트용 사용자 생성
+		if(!userRepository.existsByEmail(existedUserEmail)) {
+			String encodedPassword = passwordEncoder.encode(existedUserRawPassword);
+			User user = User.createForJoin(
+				existedUserEmail,
+				encodedPassword,
+				existedUserNickname
+			);
+			userRepository.save(user);
+			log.info("테스트용 사용자 생성: {}", user.getEmail());
+		}
+	}
 
 	@Test
 	@DisplayName("일반 회원가입 성공")
@@ -42,11 +75,6 @@ class UserControllerE2ETest {
 			UserJoinResponse.class
 		);
 
-
-		log.info("Response: {}", response);
-		log.info("Response-Body: {}", response.getBody());
-		log.info("Response-Status: {}", response.getStatusCode());
-
 		assertEquals(HttpStatus.CREATED, response.getStatusCode());
 		assertNotNull(response.getBody());
 		assertEquals("user1", response.getBody().getNickname());
@@ -54,14 +82,35 @@ class UserControllerE2ETest {
 	}
 
 	@Test
+	@DisplayName("관리자 회원가입 성공")
 	void createAdmin_success() {
 		UserJoinRequest request = new UserJoinRequest("admin1@email.com", "password2", "addmin1");
 		ResponseEntity<UserJoinResponse> response = restTemplate.postForEntity(
 			"/users/admin", request, UserJoinResponse.class);
 
-		assert response.getStatusCode() == HttpStatus.CREATED;
-		assert response.getBody() != null;
-		assert response.getBody().getNickname().equals("addmin1");
-		assert response.getBody().getRole().equals(UserRole.ADMIN);
+		assertEquals(HttpStatus.CREATED, response.getStatusCode());
+		assertNotNull(response.getBody());
+		assertEquals("addmin1", response.getBody().getNickname());
+		assertEquals(UserRole.ADMIN, response.getBody().getRole());
+	}
+
+	@Test
+	@DisplayName("로그인 성공")
+	void login_success() {
+		// 로그인 요청
+		UserLoginRequest loginRequest = new UserLoginRequest(
+			"user0@email.com",
+			"password0"
+		);
+
+		ResponseEntity<UserLoginResponse> response = restTemplate.postForEntity(
+			"/users/login", loginRequest, UserLoginResponse.class);
+
+		// 검증
+		assertEquals(HttpStatus.OK, response.getStatusCode());
+		assertNotNull(response.getBody());
+		assertEquals(existedUserEmail, response.getBody().getEmail());
+		assertEquals(existedUserNickname, response.getBody().getNickname());
+		assertEquals(UserRole.GENERAL, response.getBody().getRole());
 	}
 }
